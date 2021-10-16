ff3209b9c680c8f0f7645a43ad911012dfba20d8
prateek singh <singh-prateek@outlook.com>
2021-10-16 11:17:29 +0530

    added the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DAppComponent } from './change-detect/d-app/d-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"changedetect",
+    component: DAppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
