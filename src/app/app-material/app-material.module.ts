d098a72838f6e0b4a38ecdec95f04f81db1b0e24
Prateek Singh <singh.prateek@outlook.in>
2022-01-09 11:57:42 +0530

    added theme and default adapter

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatePickerDemoComponent } from './date-picker-demo/date-picker-demo.component';
-
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     DatePickerDemoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
   ]
 })
 export class AppMaterialModule { }
