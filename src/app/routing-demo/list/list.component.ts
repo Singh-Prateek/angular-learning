d963cb5b5ab71a3e33be8fc7c5ac583bc782f5e3
Prateek Singh <singh.prateek@outlook.in>
2022-08-14 18:49:16 +0530

    added list

--- src/app/routing-demo/list/list.component.ts
+++ src/app/routing-demo/list/list.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   template: `
-    <p>
-      list works!
-    </p>
+    <h1>Sample List of vegetables</h1>
+    <ul  *ngFor="let v of vegies">
+      <li>{{v}}</li>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class ListComponent implements OnInit {
 
+  public vegies = ['bell pepper', 'carrot', 'potato', 'cabbage', 'broccoli', 'egg plant']
   constructor() { }
 
   ngOnInit(): void {
